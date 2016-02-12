91dbc92a8e353576f9c3ba5bd3497df70bdbe7e8
andrewchosen <andrewchosen@fullsail.edu>
2016-02-11 19:18:08 -0800

    Pushing initial project to GitHub

---
+++ codeigniter/application/views/templates/footer.php
@@ -0,0 +1,7 @@
+</div>
+	</section>
+	<footer>
+		<p>&copy; 2016 Andrew Lancaster</p>
+	</footer>
+</body>
+</html>
---
+++ codeigniter/application/views/templates/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dashboard | <?php echo $title; ?></title>
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:400,300,300italic,400italic,700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/skeleton.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<h1 class="one-third column">Dashboard</h1>
+			<nav class="two-thirds column">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Messages</a></li>
+					<li><a href="#">Clients</a></li>
+					<li><a href="#">Projects</a></li>
+					<li><a href="#">Profile</a></li>
+					<li><a href="#">Logout</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section>
+		<div class="container">
